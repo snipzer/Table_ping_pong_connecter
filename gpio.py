0831f662d6752950b977d5a77e5c7c50fe996bb1
Matthieu F <fallourd.matthieu@gmail.com>
2020-02-06 10:45:29 +0100

    add buzzer and button

--- gpio.py
+++ gpio.py
@@ -3,12 +3,14 @@ import RPi.GPIO as GPIO
 
 # Définition des pins
 pinBtn = 2
+pinBuzz = 3
 
 GPIO.setmode(GPIO.BCM)
 
 # Définition des pins en entrée / sortie
 
 GPIO.setup(pinBtn, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(pinBuzz, GPIO.OUT)
 
 while True:
     etat = GPIO.input(pinBtn)
